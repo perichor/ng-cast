6b91a0ec76ccd9b610b0b3554a1c653ffb985fcd
Jeremy Sande <sandejeremy@gmail.com>
2017-04-20 15:43:50 -0700

    implement app directive

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,6 @@ angular.module('video-player')
 
 .directive('app', function() {
   return {
-    template: 'Name: {{customer.name}} Address: {{customer.address}}'
+    template: '<div></div>'
   };
 });
